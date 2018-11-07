be5293f90e907b6afccffa176b40f2fdff631364
charly <charlyp@scual.es>
2018-11-07 07:07:22 +0100

    reto4

---
+++ Assets/GameExit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameExit : MonoBehaviour {
+
+    public void SalirJuego(){
+        Debug.Log("Saliendo del juego");
+        Application.Quit();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/NuevaPartida.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NuevaPartida : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CargarNuevaPartida()
+    {
+        Debug.Log("Cargando Escena: PartidaNueva");
+        SceneManager.LoadScene("PartidaNueva");
+      
+    }
+}
